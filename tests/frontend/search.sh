75f2e45b979ea88ae4587c6dde4f3d8c1777245b
Matthew Seaman <m.seaman@infracaninophile.co.uk>
2013-05-12 20:04:48 +0100

    Attempt to make the search.sh tests more self contained.  Previously it would try and contact one of the default pkg repos, but we can't assume that any build environment will have network access. Eg. Jenkins certainly doesn't.

--- tests/frontend/search.sh
+++ tests/frontend/search.sh
@@ -3,17 +3,49 @@
 atf_test_case search
 search_head() {
 	atf_set "descr" "testing pkg search"
+        atf_set "require.files" \
+           "$(atf_get_srcdir)/png-1.5.14.yaml $(atf_get_srcdir)/repo-2005/repo.txz"
 }
 
 search_body() {
+        export PKG_DBDIR=$HOME/pkg
+	export INSTALL_AS_USER=yes
+
+        mkdir -p $PKG_DBDIR || atf_fail "can't create $PKG_DBDIR"
+
+        atf_check \
+            -o match:"^Installing png-1.5.14\.\.\." \
+            -e empty \
+            -s exit:0 \
+            pkg register -t -M $(atf_get_srcdir)/png-1.5.14.yaml
+
+        [ -f "$PKG_DBDIR/local.sqlite" ] || \
+            atf_fail "Can't populate $PKG_DBDIR/local.sqlite"
+
+	export PKG_CONF=$HOME/pkg.conf
+
+	# '2005' is the value of REPO_SCHEMA_VERSION at the time of
+	# writing.  As REPO_SCHEMA_VERSION gets updated, add new repo
+	# directories labelled with the new version so we can test 
+	# for binary compatibility...  See libpkg/pkgdb_repo.c
+
+	echo "PACKAGESITE : file:$(atf_get_srcdir)/repo-2005" > $PKG_CONF
+
+	atf_check \
+	    -e empty \
+	    -s exit:0 \
+	    pkg -C $PKG_CONF update
+
 	REPOS_DIR=/nonexistent
-	atf_check -e inline:"pkg: No repositories configured\n" -o empty -s exit:74 pkg -C '' -R '' search -e -Q comment -S name pkg
+	atf_check \
+	    -o inline:"pkg                            New generation package manager\n" \
+	    -e empty \
+	    -s exit:0 \
+	    pkg -C $PKG_CONF search -e -Q comment -S name pkg
 }
 
 atf_init_test_cases() {
         . $(atf_get_srcdir)/test_environment
-
-	unset PKG_DBDIR
 
 	atf_add_test_case search
 }
